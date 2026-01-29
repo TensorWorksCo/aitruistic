c7ee507e3d54e0fa84058159145d0a41f22f7ce3
Eric Stevens <e.stevens@tensorworks.co>
2026-01-28 16:40:49 -0800

    protect api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ import { authConfig } from "@/lib/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/admin/:path*", "/demos/:path*"],
+  matcher: ["/admin/:path*", "/demos/:path*", "/api/voice/:path*"],
 };
